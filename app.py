2ef1bb0076fbd47ca3bb9f5dea3b859a86659c78
oedunne <oedunne@gmail.com>
2026-08-15 12:13:39 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("NBA Player Value Analytics")
+
+st.write(
+    "Analyzing NBA player performance relative to salary "
+    "to identify the league's best and worst contract values."
+)
